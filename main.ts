85a741a54a460bc41a414ecbf024bb8b6e5a26a0
Leonardo Arguello <Leon-Supr@users.noreply.github.com>
2026-02-08 15:10:33 -0600

    Cumple actividad 1
    Recorre directorio, lee archivos y guarda el log de tiempos de esto en
    un archivo

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+const { readTextFile, readDir, writeTextFile } = Deno;
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+const logFile = "al_2875403.txt";
+
+try {
+  const programStart = performance.now();
+  await writeTextFile(logFile, "Log de tiempos de apertura\n\n");
+
+  for await (const entry of readDir("./Files")) {
+    if (entry.isFile && entry.name.endsWith(".html")) {
+      try {
+        const start = performance.now();
+        await readTextFile(`./Files/${entry.name}`);
+        const end = performance.now();
+
+        const time = (end - start).toFixed(4);
+        const line = `${entry.name} -> ${time} ms\n`;
+        await writeTextFile(logFile, line, { append: true });
+      } catch (err: any) {
+        const errorLine = `Error leyendo ${entry.name}: ${err.message}\n`;
+        await writeTextFile(logFile, errorLine, { append: true });
+      }
+    }
+  }
+
+  const programEnd = performance.now();
+  const totalTime = (programEnd - programStart).toFixed(4);
+  const totalTimeLine = `Tiempo total de programa: ${totalTime} ms`;
+  await writeTextFile(logFile, totalTimeLine, { append: true });
+} catch (globalError: any) {
+  console.error("ERROR CRÍTICO:", globalError.message);
 }
